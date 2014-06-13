01caf13dce676b38bfe139bdea2a718f573ec792
Tony Florida <tflorid1@jhu.edu>
2014-06-13 19:15:15 -0400

    Date picker implemented for birthday and seems to meet requirements for hw3

---
+++ florida.tony.hw3/src/florida/tony/hw3/DatePickerDialogFragment.java
@@ -0,0 +1,67 @@
+package florida.tony.hw3;
+
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.widget.DatePicker;
+
+/*
+ * Code adapted from Scott Stanchfield
+ */
+
+public class DatePickerDialogFragment extends DialogFragment {
+	public interface OnDatePickerDialogFragmentDateSetListener {
+		void onDateSet(int dateId, int year, int month, int day);
+	}
+	
+	private OnDatePickerDialogFragmentDateSetListener listener;
+	private int dateId;
+	private int fragmentId;
+	
+	public static DatePickerDialogFragment create(Fragment fragment, int dateId) {
+		return create(fragment, dateId, Calendar.getInstance());
+	}
+	public static DatePickerDialogFragment create(Fragment fragment, int dateId, Calendar calendar) {
+		if (!(fragment instanceof OnDatePickerDialogFragmentDateSetListener))
+			throw new IllegalArgumentException("Fragment must implement OnDatePickerDialogFragmentDateSetListener");
+		DatePickerDialogFragment datePickerDialogFragment = new DatePickerDialogFragment();
+		Bundle args = new Bundle();
+		args.putInt("fragmentId", fragment.getId());
+		args.putInt("dateId", dateId);
+		args.putInt("year", calendar.get(Calendar.YEAR));
+		args.putInt("month", calendar.get(Calendar.MONTH));
+		args.putInt("day", calendar.get(Calendar.DAY_OF_MONTH));
+		datePickerDialogFragment.setArguments(args);
+		return datePickerDialogFragment;
+	}
+	
+	@Override public Dialog onCreateDialog(Bundle savedInstanceState) {
+		fragmentId = getArguments().getInt("fragmentId");
+		dateId = getArguments().getInt("dateId");
+		Fragment fragment = getActivity().getSupportFragmentManager().findFragmentById(fragmentId);
+		if (fragment == null)
+			throw new IllegalStateException("No fragment with id " + fragmentId + " is available");
+		if (!fragment.isInLayout())
+			throw new IllegalStateException("No fragment with id " + fragmentId + " is in current layout");
+		if (!(fragment instanceof OnDatePickerDialogFragmentDateSetListener))
+			throw new IllegalArgumentException("Fragment must implement OnDatePickerDialogFragmentDateSetListener");
+		listener = (OnDatePickerDialogFragmentDateSetListener) fragment;
+		
+		int year = getArguments().getInt("year");
+		int day = getArguments().getInt("day");
+		int month = getArguments().getInt("month");
+		
+		DatePickerDialog dialog = new DatePickerDialog(getActivity(), callBack, year, month, day);
+		return dialog;
+	}
+	
+	private OnDateSetListener callBack = new OnDateSetListener() {
+		@Override public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+			listener.onDateSet(dateId, year, monthOfYear, dayOfMonth);
+		}};
+}
--- florida.tony.hw3/src/florida/tony/hw3/EditFragment.java
+++ florida.tony.hw3/src/florida/tony/hw3/EditFragment.java
@@ -1,7 +1,12 @@
 package florida.tony.hw3;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
+import florida.tony.hw3.DatePickerDialogFragment.OnDatePickerDialogFragmentDateSetListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,13 +24,16 @@ import android.widget.EditText;
  * Code adapted from Scott Stanchfield
  */
 
-public class EditFragment extends Fragment {
+public class EditFragment extends Fragment implements
+		OnDatePickerDialogFragmentDateSetListener {
 
 	public interface EditFragmentListener {
 		void onDone(Contact contact);
 
 		void onCancel();
 	}
+
+	private static final int BIRTH_DAY = 1;
 
 	private EditFragmentListener editFragmentListener;
 
@@ -41,7 +50,7 @@ public class EditFragment extends Fragment {
 	private EditText workPhone;
 	private EditText mobilePhone;
 	private EditText emailAddress;
-	
+
 	private Calendar birthday = Calendar.getInstance();
 
 	private Contact contact;
@@ -58,11 +67,24 @@ public class EditFragment extends Fragment {
 		displayName.setText(contact.getDisplayName());
 		firstName.setText(contact.getFirstName());
 		lastName.setText(contact.getLastName());
-		//birthday.setText(contact.getBirthday());
 		homePhone.setText(contact.getHomePhone());
 		workPhone.setText(contact.getWorkPhone());
 		mobilePhone.setText(contact.getMobilePhone());
 		emailAddress.setText(contact.getEmailAddress());
+		
+		//get text for birthday button
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+		Date birthdayAsDate;
+		try {
+			if(!contact.getBirthday().isEmpty())
+			{
+				birthdayAsDate = format.parse(contact.getBirthday());
+				birthday.setTime(birthdayAsDate);	
+			}
+			updateDateButtonText("Birthday", birthdayButton, birthday);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -80,30 +102,35 @@ public class EditFragment extends Fragment {
 		mobilePhone = (EditText) view.findViewById(R.id.mobile_phone);
 		emailAddress = (EditText) view.findViewById(R.id.email_address);
 
-		if(savedInstanceState != null)
-		{
+		if (savedInstanceState != null) {
 			birthday.setTimeInMillis(savedInstanceState.getLong("birthday"));
-		}
-		else
-		{
+		} else {
 			birthday.set(2012, 12, 12);
 		}
-		
+
 		updateDateButtonText("Birthday", birthdayButton, birthday);
 		
+		birthdayButton.setOnClickListener(new OnClickListener() {
+			@Override public void onClick(View v) {
+				DatePickerDialogFragment fragment = DatePickerDialogFragment.create(EditFragment.this, BIRTH_DAY, birthday);
+				fragment.show(getActivity().getSupportFragmentManager(), "setting birthday");
+			}});
+
 		setHasOptionsMenu(true);
 		return view;
 	}
-	
-	@Override public void onSaveInstanceState(Bundle outState) {
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putLong("birthday", birthday.getTimeInMillis());
 	}
-	
-	private void updateDateButtonText(String header, Button button, Calendar calendar) {
-		button.setText(header + ": " + calendar.get(Calendar.YEAR) + "-" + 
-								(calendar.get(Calendar.MONTH) + 1) + "-" + 
-								calendar.get(Calendar.DAY_OF_MONTH));
+
+	private void updateDateButtonText(String header, Button button,
+			Calendar calendar) {
+		button.setText(header + ": " + calendar.get(Calendar.YEAR) + "-"
+				+ (calendar.get(Calendar.MONTH) + 1) + "-"
+				+ calendar.get(Calendar.DAY_OF_MONTH));
 	}
 
 	@Override
@@ -128,10 +155,14 @@ public class EditFragment extends Fragment {
 			return true;
 		case R.id.action_done:
 			Log.d("edit frag", "done");
+			
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+			String formattedBirthday = format.format(birthday.getTime());
+			
 			this.contact.setDisplayName(displayName.getText().toString());
 			this.contact.setFirstName(firstName.getText().toString());
 			this.contact.setLastName(lastName.getText().toString());
-			//this.contact.setBirthday(birthday.getText().toString());
+			this.contact.setBirthday(formattedBirthday);
 			this.contact.setHomePhone(homePhone.getText().toString());
 			this.contact.setWorkPhone(workPhone.getText().toString());
 			this.contact.setMobilePhone(mobilePhone.getText().toString());
@@ -143,12 +174,24 @@ public class EditFragment extends Fragment {
 				throw new RuntimeException(
 						"You must set an EditFragmentListener");
 			}
-			
+
 			editFragmentListener.onDone(this.contact);
 
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onDateSet(int dateId, int year, int month, int day) {
+		switch (dateId) {
+		case BIRTH_DAY:
+			birthday.set(year, month, day);
+			updateDateButtonText("Birthday", birthdayButton, birthday);
+			break;
+		default:
+			throw new IllegalStateException("unexpected date id");
 		}
 	}
 }
