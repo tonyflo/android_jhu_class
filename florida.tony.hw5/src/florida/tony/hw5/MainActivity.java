0f485fbad564b258c7b56f0deae8d92fe048c50e
Tony Florida <tflorid1@jhu.edu>
2014-07-08 18:21:09 -0400

    Passing in vibrator object from the main activity to the puck view

--- florida.tony.hw5/src/florida/tony/hw5/MainActivity.java
+++ florida.tony.hw5/src/florida/tony/hw5/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.Display;
 import android.view.Surface;
@@ -21,88 +22,99 @@ public class MainActivity extends Activity {
 	private TextView magxView;
 	private TextView magyView;
 	private TextView magzView;
-    private Display display;
+	private Display display;
 	private PuckView puckView;
 	private SensorManager sensorManager;
 
 	SensorEventListener accelListener = new SensorEventListener() {
-		@Override public void onSensorChanged(final SensorEvent event) {
+		@Override
+		public void onSensorChanged(final SensorEvent event) {
 			runOnUiThread(new Runnable() {
-				@Override public void run() {
+				@Override
+				public void run() {
 					float x = 0;
 					float y = 0;
 					switch (display.getRotation()) {
-						case Surface.ROTATION_0:
-							x = event.values[0];
-							y = event.values[1];
-							break;
-						case Surface.ROTATION_90:
-							x = -event.values[1];
-							y = event.values[0];
-							break;
-						case Surface.ROTATION_180:
-							x = -event.values[0];
-							y = -event.values[1];
-							break;
-						case Surface.ROTATION_270:
-							x = event.values[1];
-							y = -event.values[0];
-							break;
+					case Surface.ROTATION_0:
+						x = event.values[0];
+						y = event.values[1];
+						break;
+					case Surface.ROTATION_90:
+						x = -event.values[1];
+						y = event.values[0];
+						break;
+					case Surface.ROTATION_180:
+						x = -event.values[0];
+						y = -event.values[1];
+						break;
+					case Surface.ROTATION_270:
+						x = event.values[1];
+						y = -event.values[0];
+						break;
 					}
-					xView.setText(""+x);
-					yView.setText(""+y);
-					zView.setText(""+event.values[2]);
+					xView.setText("" + x);
+					yView.setText("" + y);
+					zView.setText("" + event.values[2]);
 					puckView.change(-x * 5, y * 5);
-				}});
+				}
+			});
 		}
-		@Override public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
 			Log.d("ACCURACY CHANGE", sensor.getName() + ": " + accuracy);
 		}
 	};
 	SensorEventListener magListener = new SensorEventListener() {
-		@Override public void onSensorChanged(final SensorEvent event) {
+		@Override
+		public void onSensorChanged(final SensorEvent event) {
 			runOnUiThread(new Runnable() {
-				@Override public void run() {
+				@Override
+				public void run() {
 					float x = 0;
 					float y = 0;
 					switch (display.getRotation()) {
-						case Surface.ROTATION_0:
-							x = event.values[0];
-							y = event.values[1];
-							break;
-						case Surface.ROTATION_90:
-							x = -event.values[1];
-							y = event.values[0];
-							break;
-						case Surface.ROTATION_180:
-							x = -event.values[0];
-							y = -event.values[1];
-							break;
-						case Surface.ROTATION_270:
-							x = event.values[1];
-							y = -event.values[0];
-							break;
+					case Surface.ROTATION_0:
+						x = event.values[0];
+						y = event.values[1];
+						break;
+					case Surface.ROTATION_90:
+						x = -event.values[1];
+						y = event.values[0];
+						break;
+					case Surface.ROTATION_180:
+						x = -event.values[0];
+						y = -event.values[1];
+						break;
+					case Surface.ROTATION_270:
+						x = event.values[1];
+						y = -event.values[0];
+						break;
 					}
-					magxView.setText(""+x);
-					magyView.setText(""+y);
-					magzView.setText(""+event.values[2]);
-				}});
+					magxView.setText("" + x);
+					magyView.setText("" + y);
+					magzView.setText("" + event.values[2]);
+				}
+			});
 		}
-		@Override public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
 			Log.d("ACCURACY CHANGE", sensor.getName() + ": " + accuracy);
 		}
 	};
 	private Sensor accelerometer;
 	private Sensor magnetometer;
+	private Vibrator vibrator;
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        for (Sensor sensor : sensorList) {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+		for (Sensor sensor : sensorList) {
 			System.out.println(sensor.getName());
 			System.out.println("  VENDOR: " + sensor.getVendor());
 			System.out.println("  TYPE: " + typeName(sensor.getType()));
@@ -111,75 +123,84 @@ public class MainActivity extends Activity {
 			System.out.println("  RANGE: " + sensor.getMaximumRange());
 		}
 
-        
-        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        display = windowManager.getDefaultDisplay();
+		WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+		display = windowManager.getDefaultDisplay();
 
-        xView = (TextView) findViewById(R.id.x);
-        yView = (TextView) findViewById(R.id.y);
-        zView = (TextView) findViewById(R.id.z);
-        magxView = (TextView) findViewById(R.id.magx);
-        magyView = (TextView) findViewById(R.id.magy);
-        magzView = (TextView) findViewById(R.id.magz);
-        puckView = (PuckView) findViewById(R.id.puck);
-        
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-    }
+		xView = (TextView) findViewById(R.id.x);
+		yView = (TextView) findViewById(R.id.y);
+		zView = (TextView) findViewById(R.id.z);
+		magxView = (TextView) findViewById(R.id.magx);
+		magyView = (TextView) findViewById(R.id.magy);
+		magzView = (TextView) findViewById(R.id.magz);
+		puckView = (PuckView) findViewById(R.id.puck);
+
+		accelerometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		magnetometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+		puckView.setVibrator(vibrator);
+	}
+
 	@Override
 	protected void onResume() {
-		super.onResume();
-		sensorManager.registerListener(accelListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-		sensorManager.registerListener(magListener, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
 		puckView.start();
+		super.onResume();
+		sensorManager.registerListener(accelListener, accelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(magListener, magnetometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		
 	}
-    @Override
-    protected void onPause() {
-    	puckView.stop();
-    	sensorManager.unregisterListener(accelListener);
-    	sensorManager.unregisterListener(magListener);
-    	super.onPause();
-    }
-    private String typeName(int type) {
-    	switch (type) {
-    		case Sensor.TYPE_ACCELEROMETER:
-    			return "TYPE_ACCELEROMETER";
-    		case Sensor.TYPE_GYROSCOPE:
-    			return "TYPE_GYROSCOPE";
-    		case Sensor.TYPE_LIGHT:
-    			return "TYPE_LIGHT";
-    		case Sensor.TYPE_MAGNETIC_FIELD:
-    			return "TYPE_MAGNETIC_FIELD";
-    		case Sensor.TYPE_PRESSURE:
-    			return "TYPE_PRESSURE";
-    		case Sensor.TYPE_PROXIMITY:
-    			return "TYPE_PROXIMITY";
-    		case Sensor.TYPE_AMBIENT_TEMPERATURE:
-    			return "TYPE_AMBIENT_TEMPERATURE";
-    		case Sensor.TYPE_GRAVITY:
-    			return "TYPE_GRAVITY";
-    		case Sensor.TYPE_LINEAR_ACCELERATION:
-    			return "TYPE_LINEAR_ACCELERATION";
-    		case Sensor.TYPE_RELATIVE_HUMIDITY:
-    			return "TYPE_RELATIVE_HUMIDITY";
-    		case Sensor.TYPE_ROTATION_VECTOR:
-    			return "TYPE_ROTATION_VECTOR";
-    		case Sensor.TYPE_ORIENTATION:
-    			return "TYPE_ORIENTATION";
-    		case Sensor.TYPE_TEMPERATURE:
-    			return "TYPE_TEMPERATURE";
-    		case Sensor.TYPE_GAME_ROTATION_VECTOR:
-    			return "TYPE_GAME_ROTATION_VECTOR";
-    		case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
-    			return "TYPE_GYROSCOPE_UNCALIBRATED";
-    		case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
-    			return "TYPE_MAGNETIC_FIELD_UNCALIBRATED";
-    		case Sensor.TYPE_SIGNIFICANT_MOTION:
-    			return "TYPE_SIGNIFICANT_MOTION";
-    			
-    		default: 
-    			return "(unknown type " + type + ")";
-    	}
-    }
+
+	@Override
+	protected void onPause() {
+		puckView.stop();
+		sensorManager.unregisterListener(accelListener);
+		sensorManager.unregisterListener(magListener);
+		super.onPause();
+	}
+
+	private String typeName(int type) {
+		switch (type) {
+		case Sensor.TYPE_ACCELEROMETER:
+			return "TYPE_ACCELEROMETER";
+		case Sensor.TYPE_GYROSCOPE:
+			return "TYPE_GYROSCOPE";
+		case Sensor.TYPE_LIGHT:
+			return "TYPE_LIGHT";
+		case Sensor.TYPE_MAGNETIC_FIELD:
+			return "TYPE_MAGNETIC_FIELD";
+		case Sensor.TYPE_PRESSURE:
+			return "TYPE_PRESSURE";
+		case Sensor.TYPE_PROXIMITY:
+			return "TYPE_PROXIMITY";
+		case Sensor.TYPE_AMBIENT_TEMPERATURE:
+			return "TYPE_AMBIENT_TEMPERATURE";
+		case Sensor.TYPE_GRAVITY:
+			return "TYPE_GRAVITY";
+		case Sensor.TYPE_LINEAR_ACCELERATION:
+			return "TYPE_LINEAR_ACCELERATION";
+		case Sensor.TYPE_RELATIVE_HUMIDITY:
+			return "TYPE_RELATIVE_HUMIDITY";
+		case Sensor.TYPE_ROTATION_VECTOR:
+			return "TYPE_ROTATION_VECTOR";
+		case Sensor.TYPE_ORIENTATION:
+			return "TYPE_ORIENTATION";
+		case Sensor.TYPE_TEMPERATURE:
+			return "TYPE_TEMPERATURE";
+		case Sensor.TYPE_GAME_ROTATION_VECTOR:
+			return "TYPE_GAME_ROTATION_VECTOR";
+		case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
+			return "TYPE_GYROSCOPE_UNCALIBRATED";
+		case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
+			return "TYPE_MAGNETIC_FIELD_UNCALIBRATED";
+		case Sensor.TYPE_SIGNIFICANT_MOTION:
+			return "TYPE_SIGNIFICANT_MOTION";
+
+		default:
+			return "(unknown type " + type + ")";
+		}
+	}
 
 }
--- florida.tony.hw5/src/florida/tony/hw5/PuckView.java
+++ florida.tony.hw5/src/florida/tony/hw5/PuckView.java
@@ -1,13 +1,21 @@
 package florida.tony.hw5;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
+import android.os.Vibrator;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.TextView;
 
+/*
+ * Code adapted from Scott Stanchfield
+ */
 public class PuckView extends View {
 	private float x;
 	private float y;
@@ -17,23 +25,38 @@ public class PuckView extends View {
 	private float ay;
 	private static final int size = 50;
 	private Paint paint;
-	
+
+	// timer variables
+	private int count = 0;
+	Timer timer;
+
 	private Handler handler = new Handler();
 	private Runnable invalidator = new Runnable() {
-		@Override public void run() {
+		@Override
+		public void run() {
 			invalidate();
 		}
 	};
 	private AnimationThread animationThread;
+	private Vibrator vibrator;
+	
 
 	public PuckView(Context context) {
+		
 		super(context);
 	}
+
 	public PuckView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
+
 	public PuckView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+	}
+	
+	public void setVibrator(Vibrator vibrator)
+	{
+		this.vibrator = vibrator;
 	}
 
 	public void start() {
@@ -41,49 +64,56 @@ public class PuckView extends View {
 		animationThread = new AnimationThread();
 		animationThread.start();
 	}
+
 	public void stop() {
 		if (animationThread != null) {
 			animationThread.interrupt();
 			animationThread = null;
 		}
 	}
+
 	public void change(float ax, float ay) {
 		this.ax = ax;
 		this.ay = ay;
 	}
+
 	private class AnimationThread extends Thread {
-		@Override public void run() {
+		@Override
+		public void run() {
+			
 			while (!isInterrupted()) {
 				// accelerate
-				vx += ax; 
-				vy += ay;
+				vx += ax;
+				// vy += ay;
 
 				// move
 				x += vx;
-				y += vy;
-				
+				// y += vy;
+
 				// check bounds
 				if (x < 0) {
 					x = 0;
 					ax = 0;
-					vx = -vx/2;
+					vx = -vx / 2;
 				}
-				if (x > getWidth()-size) {
-					x = getWidth()-size;
+				if (x > getWidth() - size) {
+					x = getWidth() - size;
 					ax = 0;
-					vx = -vx/2;
-				}
-				if (y < 0) {
-					y = 0;
-					ay = 0;
-					vy = -vy/2;
-				}
-				if (y > getHeight()-size) {
-					y = getHeight()-size;
-					ay = 0;
-					vy = -vy/2;
+					vx = -vx / 2;
 				}
 				
+				//maybe check for person hitting bottom here
+				// if (y < 0) {
+				// y = 0;
+				// ay = 0;
+				// vy = -vy/2;
+				// }
+				// if (y > getHeight()-size) {
+				// y = getHeight()-size;
+				// ay = 0;
+				// vy = -vy/2;
+				// }
+
 				handler.post(invalidator);
 				try {
 					sleep(50);
@@ -93,12 +123,32 @@ public class PuckView extends View {
 			}
 		}
 	}
-	
-	@Override protected void onDraw(Canvas canvas) {
+
+	@Override
+	protected void onDraw(Canvas canvas) {
 		if (paint == null) {
 			paint = new Paint();
 			paint.setColor(Color.RED);
+
 		}
-		canvas.drawRect(x,y,x+size,y+size, paint);
+		canvas.drawRect(x, y, x + size, y + size, paint);
+		
+	}
+	
+	boolean checkHit()
+	{
+		//if hit is true
+		if(true)
+		{
+			//move the person down a bit
+			y+=size;
+			
+			//vibrate
+			vibrator.vibrate(100);
+			
+			return true;
+		}
+		
+		return false;
 	}
 }
