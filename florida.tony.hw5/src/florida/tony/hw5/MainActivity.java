45172a6563b2bd6fe8d995bae9e8c62d0cf390fc
Tony Florida <tflorid1@jhu.edu>
2014-07-23 21:54:39 -0400

    hw 5 submission

--- florida.tony.hw5/src/florida/tony/hw5/MainActivity.java
+++ florida.tony.hw5/src/florida/tony/hw5/MainActivity.java
@@ -21,7 +21,6 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.Surface;
